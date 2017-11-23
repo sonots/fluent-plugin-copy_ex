3940d9b8b035a99065c7ecaaa24390df241c5481
Andras Suller <andras.suller@bitgamelabs.com>
2017-11-23 16:52:43 +0800

    Make it compatible with fluentd 0.14

--- lib/fluent/plugin/out_copy_ex.rb
+++ lib/fluent/plugin/out_copy_ex.rb
@@ -1,71 +1,43 @@
-require 'fluent/output'
+require 'fluent/plugin/multi_output'
+require 'fluent/config/error'
+require 'fluent/event'
 
-module Fluent
+module Fluent::Plugin
   class CopyExOutput < MultiOutput
-    Plugin.register_output('copy_ex', self)
+    Fluent::Plugin.register_output('copy_ex', self)
 
-    config_param :deep_copy, :bool, :default => false
+    desc 'If true, pass different record to each `store` plugin.'
+    config_param :deep_copy, :bool, default: false
 
     def initialize
       super
-      @outputs = []
       @ignore_errors = []
-      @emit_procs = []
     end
 
-    attr_reader :outputs, :ignore_errors
+    attr_reader :ignore_errors
 
     def configure(conf)
       super
       conf.elements.select {|e|
         e.name == 'store'
       }.each {|e|
-        type = e['@type'] || e['type']
-        unless type
-          raise ConfigError, "Missing '@type' parameter on <store> directive"
-        end
-        log.debug "adding store type=#{type.dump}"
-
-        output = Plugin.new_output(type)
-        output.configure(e)
-        emit_proc = if output.respond_to?(:emit_events)
-                      Proc.new {|output, tag, es, _chain| output.emit_events(tag, es)}
-                    else
-                      Proc.new {|output, tag, es, _chain| output.emit(tag, es, NullOutputChain.instance)}
-                    end
-        @emit_procs << emit_proc
-        @outputs << output
-
         @ignore_errors << (e.arg == "ignore_error")
       }
     end
 
-    def start
-      @outputs.each {|o|
-        o.start
-      }
-    end
-
-    def shutdown
-      @outputs.each {|o|
-        o.shutdown
-      }
-    end
-
-    def emit(tag, es, chain)
+    def process(tag, es)
       unless es.repeatable?
-        m = MultiEventStream.new
+        m = Fluent::MultiEventStream.new
         es.each {|time,record|
           m.add(time, record)
         }
         es = m
       end
 
-      # Here, we do not use OutputChain for custom
-      @outputs.each_index do |idx|
+      outputs.each_with_index do |output, idx|
         _es = @deep_copy ? es.dup : es
         begin
-          @emit_procs[idx].call(@outputs[idx], tag, _es, NullOutputChain.instance)
+          output.emit_events(tag, @deep_copy ? es.dup : es)
         rescue => e
           if @ignore_errors[idx]
             log.error :error_class => e.class, :error => e.message
@@ -74,8 +46,6 @@ module Fluent
           end
         end
       end
-
-      chain.next
     end
   end
 end
