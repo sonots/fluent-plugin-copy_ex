e49a060ffdb199d3ce93ea5048eed88a07d5267c
sonots <sonots@gmail.com>
2017-11-24 21:35:45 +0900

    simplify (v0.12)

--- lib/fluent/plugin/out_copy_ex.rb
+++ lib/fluent/plugin/out_copy_ex.rb
@@ -1,54 +1,21 @@
-require 'fluent/output'
+require 'fluent/plugin/out_copy'
 
 module Fluent
-  class CopyExOutput < MultiOutput
+  class CopyOutputEx < CopyOutput
     Plugin.register_output('copy_ex', self)
-
-    config_param :deep_copy, :bool, :default => false
 
     def initialize
       super
-      @outputs = []
       @ignore_errors = []
-      @emit_procs = []
     end
-
-    attr_reader :outputs, :ignore_errors
 
     def configure(conf)
       super
+
       conf.elements.select {|e|
         e.name == 'store'
-      }.each {|e|
-        type = e['@type'] || e['type']
-        unless type
-          raise ConfigError, "Missing '@type' parameter on <store> directive"
-        end
-        log.debug "adding store type=#{type.dump}"
-
-        output = Plugin.new_output(type)
-        output.configure(e)
-        emit_proc = if output.respond_to?(:emit_events)
-                      Proc.new {|output, tag, es, _chain| output.emit_events(tag, es)}
-                    else
-                      Proc.new {|output, tag, es, _chain| output.emit(tag, es, NullOutputChain.instance)}
-                    end
-        @emit_procs << emit_proc
-        @outputs << output
-
+      }.each.with_index {|e, i|
         @ignore_errors << (e.arg == "ignore_error")
-      }
-    end
-
-    def start
-      @outputs.each {|o|
-        o.start
-      }
-    end
-
-    def shutdown
-      @outputs.each {|o|
-        o.shutdown
       }
     end
 
@@ -65,7 +32,7 @@ module Fluent
       @outputs.each_index do |idx|
         _es = @deep_copy ? es.dup : es
         begin
-          @emit_procs[idx].call(@outputs[idx], tag, _es, NullOutputChain.instance)
+          @outputs[idex].emit(tag, _es, NullOutputChain.instance)
         rescue => e
           if @ignore_errors[idx]
             log.error :error_class => e.class, :error => e.message
